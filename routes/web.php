d4575dd6332330f250c3243720f44a4067d67425
RizkyFitriAndini <fitriandini457@gmail.com>
2024-03-16 23:06:37 +0700

    Query Builder

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\PhotoController;
@@ -25,3 +26,4 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
