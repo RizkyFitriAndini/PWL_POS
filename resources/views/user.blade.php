7082fbe6ac8ba278fd50f0c025f5d1c4385690ee
RizkyFitriAndini <fitriandini457@gmail.com>
2024-03-17 02:19:34 +0700

    Praktikum 2.1 - J4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Data User</title>
 </head>
 <body>
@@ -15,14 +11,12 @@
             <th>Nama</th>
             <th>ID Level Pengguna</th>
         </tr>
-        @foreach ($data as $d)
         <tr>
-            <td>{{$d->user_id}}</td>
-            <td>{{$d->username}}</td>
-            <td>{{$d->nama}}</td>
-            <td>{{$d->level_id}}</td>
+            <td>{{$data->user_id}}</td>
+            <td>{{$data->username}}</td>
+            <td>{{$data->nama}}</td>
+            <td>{{$data->level_id}}</td>
         </tr>
-        @endforeach
     </table>
 </body>
 </html>
