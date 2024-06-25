23aeccfb87c97ab34e9f6880d7b13cc0ced472aa
RizkyFitriAndini <fitriandini457@gmail.com>
2024-06-25 20:58:33 +0700

    J12 - Praktikum 4

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -10,29 +10,11 @@ class FileUploadController extends Controller
         return view('file-upload');
     }
     public function prosesFileUpload(Request $request) {
-        // return "Pemrosesan File Upload di sini";
-        if($request->hasFile('berkas'))
-    {
-        echo "path(): ".$request->berkas->path();
-        echo "<br>";
-        echo "extension(): ".$request->berkas->extension();
-        echo "<br>";
-        echo "getClientOriginalExtension(): ".
-             $request->berkas->getClientOriginalExtension();
-        echo "<br>";
-        echo "getMimeType(): ".$request->berkas->getMimeType();
-        echo "<br>";
-        echo "getClientOriginalName(): ".
-             $request->berkas->getClientOriginalName();
-        echo "<br>";
-        echo "getSize(): ".$request->berkas->getSize();
-    }
-    else 
-    {
-        echo "Tidak ada berkas yang diupload";
-    }
-    $request->validate([
-        'berkas'=>'required|file|image|max:5000']);
-        echo $request->berkas->getClientOriginalName()."Lolos Validasi";
+        $request->validate([
+            'berkas'=>'required|file|image|max:500',]);
+            $extfile =  $request->berkas->getClientOriginalName();
+            $namaFile = 'web-'.time().".".$extfile;
+            $path = $request->berkas->storeAs('uploads',$namaFile);
+            echo "Proses upload berhasil, data disimpan pada: $path";
     }
 }
